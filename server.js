ec5bbf25f4ebb7a5116d17bab6fa72cfe3ddd66e
harshaa <124580382+theHarshaa@users.noreply.github.com>
2023-06-28 19:33:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 //routes
 app.use('/api/v1/auth', authRoutes)
@@ -36,7 +36,7 @@ app.use('/api/v1/product', productRoute)
 
 //rest api
 app.use('*', function(req,res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
 
 //PORT
