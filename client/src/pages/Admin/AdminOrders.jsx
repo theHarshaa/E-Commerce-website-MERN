a5ec93e829b7be51241621646134a4a0c41b9c65
harshaa <124580382+theHarshaa@users.noreply.github.com>
2023-08-16 12:40:19 +0530

    /

---
+++ client/src/pages/Admin/AdminOrders.jsx
@@ -0,0 +1,106 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+// import toast from 'react-hot-toast'
+import Layout from '../../components/layouts/Layout'
+import AdminMenu from '../../components/layouts/AdminMenu'
+import { useAuth } from '../../context/auth'
+import moment from 'moment'
+import { Select } from 'antd'
+const { Option } = Select;
+export default function AdminOrders() {
+  // eslint-disable-next-line
+    const [status,setStatus] = useState(["Not Process", "Processing", "Shipped", "Deliverd", "Cancel"])
+    const [orders,setOrders] = useState([])
+  // eslint-disable-next-line
+  const [auth, setAuth] = useAuth()
+
+  const getOrders = async() => {
+    try {
+      const {data} = await axios.get(`/api/v1/auth/all-orders`)
+      setOrders(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    if(auth?.token) getOrders()
+  },[auth?.token])
+
+  const handleChange = async(orderId,value) => {
+    try {
+      // eslint-disable-next-line
+      const {data} =await axios.put(`/api/v1/auth/order-status/${orderId}`,{status:value})
+      getOrders()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Layout title={'All Order Data'}>
+    <div className='container-fluid m-3 p-4'>
+    <div className='row'>
+        <div className='col-md-2 flex justify-center'>
+            <AdminMenu />
+        </div>
+        <div className='col-md-10'>
+            <h1 className='text-center text-3xl'>All Order</h1>
+            {
+              orders?.map((o,i) => {
+                return(
+                  <div className='border shadow justify-center'>
+                    <table className='table'>
+                      <thead>
+                        <tr>
+                          <th scope='col'>#</th>
+                          <th scope='col'>Status</th>
+                          <th scope='col'>Buyer</th>
+                          <th scope='col'>Time</th>
+                          <th scope='col'>Payment</th>
+                          <th scope='col'>Quantity</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                        <td>{i+1}</td>
+                        <td>
+                            <Select bordered={false} onChange={(value) => handleChange(o?._id,value)} defaultValue={o?.status}>
+                                {status.map((s,i) => (
+                                  <Option key={i} value={s}>{s}</Option>
+                                ))}
+                            </Select>
+                        </td>
+                        <td>{o?.buyer?.name}</td>
+                        <td>{moment(o?.createAt).fromNow()}</td>
+                        <td>{o?.payment.success ? "Success" : "Failed"}</td>
+                        <td>{o?.products?.length}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className='container'>
+                    {o?.products?.map((p,i) => (
+                     <div className='row m-2 card p-3 flex-row'>
+                    <div className='col-md-3'>
+                    <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top w-75" alt={p.name}/>
+                     </div>
+                    <div className='col-md-9'>
+                        <h1 className='text-2xl'>{p.name}</h1>
+                        <p className='text-sm'>{p.description}</p>
+                        <h4 className='text-base'>Price : {p.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}</h4>
+                        </div>
+                </div>
+               ))}
+                    </div>
+                  </div>
+                )
+              })
+            }
+        </div>
+      </div>
+    </div>
+    </Layout>
+  )
+}
---
+++ client/src/pages/Admin/CreateProduct.jsx
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/layouts/Layout";
+import AdminMenu from "../../components/layouts/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Select } from "antd";
+import { useNavigate } from "react-router-dom";
+const { Option } = Select;
+
+const CreateProduct = () => {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  // eslint-disable-next-line
+  const [ shipping,setShipping] = useState("");
+  const [image, setImage] = useState("");
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(`/api/v1/category/get-category`);
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something wwent wrong in getting catgeory");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //create product function
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    try {
+      const productData = new FormData();
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("quantity", quantity);
+      productData.append("image", image);
+      productData.append("category", category);
+      const { data } = axios.post(
+        `/api/v1/product/create-product`,
+        productData
+      );
+      if (data?.success) {
+        toast.error(data?.message);
+      } else {
+        toast.success("Product Created Successfully");
+        navigate("/dashboard/admin/products");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong");
+    }
+  };
+
+  return (
+    <Layout title={"Dashboard - Create Product"}>
+      <div className="container-fluid m-3 p-4">
+        <div className="row">
+          <div className="col-md-2 flex justify-center">
+            <AdminMenu />
+          </div>
+          <div className="col-md-10  justify-center align-center text-center items-center">
+            <h1 className='text-3xl mb-2 mt-3'>Create Product</h1>
+            <div className="m-1">
+              <Select
+                placeholder="Select a category"
+                size="large"
+                showSearch
+                className="form-select mb-3 w-full"
+                onChange={(value) => {
+                  setCategory(value);
+                }}
+              >
+                {categories?.map((c) => (
+                  <Option key={c._id} value={c._id}>
+                    {c.name}
+                  </Option>
+                ))}
+              </Select>
+              <div className="mb-3">
+                <label className="btn btn-outline-secondary col-md-12">
+                  {image ? image.name : "Upload Image"}
+                  <input
+                    type="file"
+                    name="image"
+                    accept="image/*"
+                    onChange={(e) => setImage(e.target.files[0])}
+                    hidden
+                  />
+                </label>
+              </div>
+              <div className="mb-3">
+                {image && (
+                  <div className="justify-center flex">
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt="product_image"
+                      className="img w-25 img-responsive"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="write a name"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  value={description}
+                  placeholder="write a description"
+                  className="form-control"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={price}
+                  placeholder="write a Price"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={quantity}
+                  placeholder="write a quantity"
+                  className="form-control"
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <Select
+                  placeholder="Select Shipping "
+                  size="large"
+                  showSearch
+                  className="form-select mb-3 w-full"
+                  onChange={(value) => {
+                    setShipping(value);
+                  }}
+                >
+                  <Option value="0">No</Option>
+                  <Option value="1">Yes</Option>
+                </Select>
+              </div>
+              <div className="mb-3">
+                <button className="btn btn-primary" onClick={handleCreate}>
+                  Create Product
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CreateProduct;
